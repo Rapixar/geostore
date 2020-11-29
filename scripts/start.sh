25b05c8df647a7011f4a4f8c0f437188b38e8082
rapixar <cudraphael@gmail.com>
2020-11-29 10:01:59 +0100

    Postgres automation

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,5 +2,17 @@
 #!/usr/bin/env bash
 
 echo "deploying application"
-sudo java *.war
+
+sudo -u postgres createdb geostore
+sudo -u postgres psql -d geostore -f ./doc/sql/001_setup_db.sql
+sudo -u postgres psql -U geostore -d geostore -f ./doc/sql/002_create_schema_postgres.sql
+
+cd src
+mvn clean install -Dovrdir=postgres -Ppostgres
+cd /src/web/app
+mvn jetty:run
+
+sudo systemctl restart tomcat
+
+
 echo "done deploying application"
